7e473fbd3bd5dc45c02398a4995aeee2c82064b3
Santiago Nieto <stiago.r89@gmail.com>
2025-04-06 01:50:26 -0500

    Primera versión del formulario de contacto

---
+++ js/script.js
@@ -0,0 +1,4 @@
+document.getElementById('formulario-contacto').addEventListener('submit', function(event) {
+    event.preventDefault();
+    alert('Formulario enviado correctamente (simulado)');
+});
